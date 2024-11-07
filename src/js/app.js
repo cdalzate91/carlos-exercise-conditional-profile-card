edeb3f6eabea323178d4694ceb9051598e8f2883
Carlos Alzate <158311146+cdalzate91@users.noreply.github.com>
2024-11-07 01:37:51 +0000

    Nombre

--- src/js/app.js
+++ src/js/app.js
@@ -28,7 +28,11 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  const name = variables.name ? variables.name : "Aquí aparecerá tu nombre";
+  let name = `<h1>${variables.name}</h1>`;
+
+  if (variables.name == null) name = <h1>Aquí aparecerás tu</h1>;
+  // const name = variables.name ? variables.name : "Aquí aparecerá tu nombre";
+
   const lastName = variables.lastName
     ? variables.lastName
     : "Aquí aparecerá tu apellido";
@@ -36,15 +40,15 @@ function render(variables = {}) {
   const city = variables.city ? variables.city : "Aquí aparecerá tu Ciudad";
   const country = variables.country
     ? variables.country
-    : "Aquí aparecerá tu nombre";
+    : "Aquí aparecerá tu País";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${name} ${lastName}</h1>
           <h2>${role}</h2>
-          <h3>${city}, ${variables.country}</h3>
-          <ul class="position-right">
+          <h3>${city}, ${country}</h3>
+          <ul class="position-left">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
